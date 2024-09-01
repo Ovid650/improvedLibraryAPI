e80f6c88cd5cb6f313d55697bfc095756333a1f3
Ovid650 <ovidiutocan6@gmail.com>
2024-09-01 12:27:23 +0300

    Add files via upload

---
+++ MIddleware/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿public class RequestResponseLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<RequestResponseLoggingMiddleware> _logger;
+
+    public RequestResponseLoggingMiddleware(RequestDelegate next, ILogger<RequestResponseLoggingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Log Request
+        _logger.LogInformation($"Incoming Request: {context.Request.Method} {context.Request.Path}");
+
+        
+        var originalBodyStream = context.Response.Body;
+
+        using (var responseBody = new MemoryStream())
+        {
+            context.Response.Body = responseBody;
+
+            await _next(context);
+
+            // Log Response
+            _logger.LogInformation($"Outgoing Response: {context.Response.StatusCode}");
+
+            await responseBody.CopyToAsync(originalBodyStream);
+        }
+    }
+}
